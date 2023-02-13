9b0fd3affcf74792b6aa71e4fe4fb5888fea3ff9
Duy Phuc <iamndphuc@gmail.com>
2023-02-13 10:23:33 +0700

    finish video 03

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,10 +6,21 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private Rigidbody rb;
     [SerializeField] private float forwardForce = 4000f;
+    [SerializeField] private float sidewaysForce = 500f;
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        rb.AddForce(0, 0, forwardForce * Time.deltaTime);    
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        
+        if(Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
     }
 }
